5cf18ec67ff0435096a36fd6bdb3afcce4b7dbcf
julianes <garzon3@hotmail.com>
2016-04-23 20:21:50 +0200

    april23 update

--- myMap.js
+++ myMap.js
@@ -162,3 +162,20 @@ $('button').on('click', function(){
 					});
     		});
 });
+
+
+
+// var min = 12,
+//     max = 100,
+//     select = document.getElementById('countries');
+
+// for (var i = min; i<=max; i++){
+//     var opt = document.createElement("option");
+//     opt.value = i;
+//     opt.innerHTML = i;
+//     select.appendChild(opt);
+// }
+
+
+
+
